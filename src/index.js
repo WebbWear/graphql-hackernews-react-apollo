a5e000459a9b2e4006a72095b704c85245cbbb97
Jeff Webb <webbwear@icloud.com>
2020-09-30 17:31:42 -0400

    Routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+
 
 // 1
 import { ApolloProvider } from 'react-apollo'
@@ -24,9 +26,12 @@ const client = new ApolloClient({
 
 // 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
+
 serviceWorker.unregister();
