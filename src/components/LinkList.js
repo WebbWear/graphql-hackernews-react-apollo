bf94df03a6fa61cf2f978e299946e2b538d3c129
Jeff Webb <webbwear@icloud.com>
2020-10-01 17:28:37 -0400

    Mutations and Updating the Store

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,7 +4,7 @@ import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -12,6 +12,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -19,6 +29,15 @@ const FEED_QUERY = gql`
 
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+  
     render() {
       return (
         <Query query={FEED_QUERY}>
@@ -30,7 +49,14 @@ class LinkList extends Component {
       
             return (
               <div>
-                {linksToRender.map(link => <Link key={link.id} link={link} />)}
+                {linksToRender.map((link, index) => (
+                  <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                  />
+                ))}
               </div>
             )
           }}
