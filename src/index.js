98be30e1a81b01926f54a1ad00ce0c4063f4a887
Jeff Webb <webbwear@icloud.com>
2020-10-02 14:41:11 -0400

    Realtime Updates with GraphQL Subscriptions

--- src/index.js
+++ src/index.js
@@ -2,49 +2,62 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
-import { setContext } from 'apollo-link-context'
-
-
-// 1
+import * as serviceWorker from './serviceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
 import { AUTH_TOKEN } from './constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem(AUTH_TOKEN)
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
+      authorization: token ? `Bearer ${token}` : '',
+    },
   }
 })
 
-
-
-// 2
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
 })
 
-// 3
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink),
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  link,
+  cache: new InMemoryCache(),
 })
 
-// 4
 ReactDOM.render(
   <BrowserRouter>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
-
-serviceWorker.unregister();
+serviceWorker.unregister()
